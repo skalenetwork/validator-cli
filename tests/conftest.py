d51d02029b2ab74c7cd017c579a11ff667140d80
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-17 18:08:41 +0200

    SKALE-2195 Cover all validator module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """ SKALE config test """
 
 import pytest
+from click.testing import CliRunner
 
 from utils.web3_utils import init_skale_w_wallet_from_config
 from tests.constants import TEST_PK_FILE
@@ -10,3 +11,8 @@ from tests.constants import TEST_PK_FILE
 def skale():
     '''Returns a SKALE instance with provider from config'''
     return init_skale_w_wallet_from_config(pk_file=TEST_PK_FILE)
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
