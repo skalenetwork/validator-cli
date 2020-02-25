010079469bf4fc6cf6e94d4341feef9eb5d7fe62
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2020-02-25 17:57:49 +0200

    SKALE-2241 Add branch to upload script

--- scripts/upload_to_do.sh
+++ scripts/upload_to_do.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python ./scripts/upload_to_do.py ./dist/$EXECUTABLE_NAME validator-cli ./$EXECUTABLE_NAME
+python ./scripts/upload_to_do.py ./dist/$EXECUTABLE_NAME validator-cli $BRANCH/$EXECUTABLE_NAME
