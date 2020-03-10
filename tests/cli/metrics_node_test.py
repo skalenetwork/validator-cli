20ecf4dbc8dd655617a70ac637b51609d91d9e29
Alex Sheverdin <alex@skalelabs.com>
2020-03-10 17:33:45 +0200

    SKALE-2263 Add 'no data' message and update tests

--- tests/cli/metrics_node_test.py
+++ tests/cli/metrics_node_test.py
@@ -1,11 +1,14 @@
 """ Tests for cli/metrics.py module """
 
-from cli.metrics import node
-
-from tests.constants import NODE_ID, BOUNTY_COUNT, SERVICE_ROW_COUNT
-
-from core.metrics import get_metrics_from_events
 from datetime import datetime
+
+from cli.metrics import node
+from core.metrics import get_metrics_from_events
+from tests.constants import NODE_ID, SERVICE_ROW_COUNT
+from utils.texts import Texts
+
+G_TEXTS = Texts()
+NO_DATA_MSG = G_TEXTS['msg']['no_data']
 
 
 def yy_mm_dd_to_date(date_str):
@@ -14,16 +17,13 @@ def yy_mm_dd_to_date(date_str):
 
 
 def test_metrics(runner):
-    result = runner.invoke(node, ['-id', str(NODE_ID)])
-    row_count = BOUNTY_COUNT + SERVICE_ROW_COUNT
     metrics, total_bounty = get_metrics_from_events([NODE_ID])
-    print(metrics)
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    result = runner.invoke(node, ['-id', str(NODE_ID)])
     output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
-    print(len(output_list))
+
     assert '       Date           Bounty   Downtime   Latency' == output_list[0]
     assert '-------------------------------------------------' == output_list[1]
-
     assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[2]  # noqa
     assert f'{metrics[1][0]}    {metrics[1][1]:.1f}          {metrics[1][2]}       {metrics[1][3]:.1f}' == output_list[3]  # noqa
     assert '' == output_list[-2]
@@ -32,11 +32,10 @@ def test_metrics(runner):
 
 def test_metrics_limited(runner):
     metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1)
-    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1)])
     row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1)])
     output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
+
     assert '       Date           Bounty   Downtime   Latency' == output_list[0]
     assert '-------------------------------------------------' == output_list[1]
     assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[2]  # noqa
@@ -48,11 +47,10 @@ def test_metrics_since_limited_not_empty(runner):
     start_date = '2000-01-01'
     metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
                                                     start_date=yy_mm_dd_to_date(start_date))
-    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-s', start_date])
     row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-s', start_date])
     output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
+
     assert '       Date           Bounty   Downtime   Latency' == output_list[0]
     assert '-------------------------------------------------' == output_list[1]
     assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[2]  # noqa
@@ -62,28 +60,20 @@ def test_metrics_since_limited_not_empty(runner):
 
 def test_metrics_since_limited_empty(runner):
     start_date = '2100-01-01'
-    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
-                                                    start_date=yy_mm_dd_to_date(start_date))
     result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-s', start_date])
-    row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics, total_bounty)
-    output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
-    assert 'Date   Bounty   Downtime   Latency' == output_list[0]
-    assert '----------------------------------' == output_list[1]
-    assert '' == output_list[-2]
-    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+    output_list = result.output.splitlines()
+
+    assert NO_DATA_MSG == output_list[-1]
 
 
 def test_metrics_till_limited_not_empty(runner):
     end_date = '2100-01-01'
     metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
                                                     end_date=yy_mm_dd_to_date(end_date))
-    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
     row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
     output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
+
     assert '       Date           Bounty   Downtime   Latency' == output_list[0]
     assert '-------------------------------------------------' == output_list[1]
     assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[2]  # noqa
@@ -93,17 +83,10 @@ def test_metrics_till_limited_not_empty(runner):
 
 def test_metrics_till_limited_empty(runner):
     end_date = '2000-01-01'
-    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
-                                                    end_date=yy_mm_dd_to_date(end_date))
     result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
-    row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
-    output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
-    assert 'Date   Bounty   Downtime   Latency' == output_list[0]
-    assert '----------------------------------' == output_list[1]
-    assert '' == output_list[-2]
-    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+    output_list = result.output.splitlines()
+
+    assert NO_DATA_MSG == output_list[-1]
 
 
 def test_metrics_since_till_limited_not_empty(runner):
@@ -112,11 +95,10 @@ def test_metrics_since_till_limited_not_empty(runner):
     metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
                                                     start_date=yy_mm_dd_to_date(start_date),
                                                     end_date=yy_mm_dd_to_date(end_date))
-    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
     row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
     output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
+
     assert '       Date           Bounty   Downtime   Latency' == output_list[0]
     assert '-------------------------------------------------' == output_list[1]
     assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[2]  # noqa
@@ -127,15 +109,7 @@ def test_metrics_since_till_limited_not_empty(runner):
 def test_metrics_since_till_limited_empty(runner):
     start_date = '2100-01-01'
     end_date = '2100-02-01'
-    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
-                                                    start_date=yy_mm_dd_to_date(start_date),
-                                                    end_date=yy_mm_dd_to_date(end_date))
-    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
-    row_count = len(metrics) + SERVICE_ROW_COUNT
-    print(metrics)
-    output_list = result.output.splitlines()[-row_count:]
-    print(output_list)
-    assert 'Date   Bounty   Downtime   Latency' == output_list[0]
-    assert '----------------------------------' == output_list[1]
-    assert '' == output_list[-2]
-    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1),
+                                  '-s', start_date, '-t', end_date])
+    output_list = result.output.splitlines()
+    assert NO_DATA_MSG == output_list[-1]
