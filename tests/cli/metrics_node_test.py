5787af86a7b976dbef183278345b9a41e537f9fc
Alex Sheverdin <alex@skalelabs.com>
2020-03-10 14:24:20 +0200

    SKALE-2263 Write node metrics command tests

---
+++ tests/cli/metrics_node_test.py
@@ -0,0 +1,109 @@
+""" Tests for cli/metrics.py module """
+
+from cli.metrics import node
+
+from tests.constants import NODE_ID, BOUNTY_COUNT, SERVICE_ROW_COUNT
+
+from core.metrics import get_metrics_from_events
+from datetime import datetime
+
+
+def get_bounty(skale, node_id):
+    skale.manager.get_bounty(node_id, wait_for=True)
+
+
+def yy_mm_dd_to_date(date_str):
+    format_str = '%Y-%m-%d'
+    return datetime.strptime(date_str, format_str)
+
+
+def test_metrics(runner):
+    result = runner.invoke(node, ['-id', str(NODE_ID)])
+    row_count = BOUNTY_COUNT + SERVICE_ROW_COUNT
+    metrics, total_bounty = get_metrics_from_events([NODE_ID])
+    print(metrics)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert '       Date           Bounty   Downtime   Latency' == output_list[-6]
+    assert '-------------------------------------------------' == output_list[-5]
+
+    assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[-4]  # noqa
+    assert f'{metrics[1][0]}    {metrics[1][1]:.1f}          {metrics[1][2]}       {metrics[1][3]:.1f}' == output_list[-3]  # noqa
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+
+
+def test_metrics_limited(runner):
+    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1)
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1)])
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    print(metrics)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert '       Date           Bounty   Downtime   Latency' == output_list[-5]
+    assert '-------------------------------------------------' == output_list[-4]
+    assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[-3]  # noqa
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+
+
+def test_metrics_since_not_empty(runner):
+    start_date = '2000-01-01'
+    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
+                                                    start_date=yy_mm_dd_to_date(start_date))
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-s', start_date])
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    print(metrics)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert '       Date           Bounty   Downtime   Latency' == output_list[-5]
+    assert '-------------------------------------------------' == output_list[-4]
+    assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[-3]  # noqa
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+
+
+def test_metrics_since_empty(runner):
+    start_date = '2100-01-01'
+    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
+                                                    start_date=yy_mm_dd_to_date(start_date))
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-s', start_date])
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    print(metrics, total_bounty)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert 'Date   Bounty   Downtime   Latency' == output_list[-4]
+    assert '----------------------------------' == output_list[-3]
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+
+
+def test_metrics_till_not_empty(runner):
+    end_date = '2100-01-01'
+    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
+                                                    end_date=yy_mm_dd_to_date(end_date))
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    print(metrics)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert '       Date           Bounty   Downtime   Latency' == output_list[-5]
+    assert '-------------------------------------------------' == output_list[-4]
+    assert f'{metrics[0][0]}    {metrics[0][1]:.1f}          {metrics[0][2]}       {metrics[0][3]:.1f}' == output_list[-3]  # noqa
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
+
+
+def test_metrics_till_empty(runner):
+    end_date = '2000-01-01'
+    metrics, total_bounty = get_metrics_from_events([NODE_ID], limit=1,
+                                                    end_date=yy_mm_dd_to_date(end_date))
+    result = runner.invoke(node, ['-id', str(NODE_ID), '-l', str(1), '-t', end_date])
+    row_count = len(metrics) + SERVICE_ROW_COUNT
+    print(metrics)
+    output_list = result.output.splitlines()[-row_count:]
+    print(output_list)
+    assert 'Date   Bounty   Downtime   Latency' == output_list[-4]
+    assert '----------------------------------' == output_list[-3]
+    assert '' == output_list[-2]
+    assert f' Total bounty per the given period: {total_bounty:.3f} SKL' == output_list[-1]  # noqa
--- tests/prepare_data.py
+++ tests/prepare_data.py
@@ -1,12 +1,68 @@
 """ Preparation scripts for tests """
 
-from skale.utils.helper import init_default_logger
+import random
+import string
+import time
+
 from skale.utils.contracts_provision.main import setup_validator
-from tests.constants import TEST_PK_FILE
+from skale.utils.helper import init_default_logger
+
+from tests.constants import NODE_ID, TEST_DELTA, TEST_EPOCH, TEST_NODE_NAME, TEST_PK_FILE
 from utils.web3_utils import init_skale_w_wallet_from_config
+
+
+def accelerate_skale_manager(skale):
+
+    reward_period = skale.constants_holder.get_reward_period()
+    delta_period = skale.constants_holder.get_delta_period()
+    print(f'Existing times for SM: {reward_period}, {delta_period}')
+
+    tx_res = skale.constants_holder.set_periods(TEST_EPOCH, TEST_DELTA, wait_for=True)
+    assert tx_res.receipt['status'] == 1
+    print(tx_res.receipt)
+    reward_period = skale.constants_holder.get_reward_period()
+    delta_period = skale.constants_holder.get_delta_period()
+    print(f'New times for SM: {reward_period}, {delta_period}')
+
+
+def generate_random_ip():
+    return '.'.join('%s' % random.randint(0, 255) for i in range(4))
+
+
+def generate_random_name(len=8):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=len))
+
+
+def generate_random_port():
+    return random.randint(0, 60000)
+
+
+def generate_random_node_data():
+    return generate_random_ip(), generate_random_ip(), generate_random_port(), \
+        generate_random_name()
+
+
+def create_nodes(skale, nodes_count):
+    # create couple of nodes
+    print(f'Creating {nodes_count} nodes')
+    node_names = [TEST_NODE_NAME + str(i) for i in range(nodes_count)]
+    for name in node_names:
+        ip, public_ip, port, _ = generate_random_node_data()
+        skale.manager.create_node(ip, port, name, public_ip, wait_for=True)
+
+
+def get_bounties(skale):
+    time.sleep(TEST_EPOCH + 5)
+    skale.manager.get_bounty(NODE_ID, wait_for=True)
+    skale.manager.get_bounty(NODE_ID + 1, wait_for=True)
+    time.sleep(TEST_EPOCH + 5)
+    skale.manager.get_bounty(NODE_ID, wait_for=True)
 
 
 if __name__ == "__main__":
     init_default_logger()
     skale = init_skale_w_wallet_from_config(pk_file=TEST_PK_FILE)
     setup_validator(skale)
+    accelerate_skale_manager(skale)
+    create_nodes(skale, 2)
+    get_bounties(skale)
