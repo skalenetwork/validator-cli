ffed14e88669492c9a87d2fd7d08d0a37ecbe108
Alex Sheverdin <alex@skalelabs.com>
2020-04-30 17:18:45 +0300

    SKALE-2491 Fix after 'startDate -> startBlock'

--- core/metrics.py
+++ core/metrics.py
@@ -44,14 +44,16 @@ def progress_bar(count, total, status='', bar_len=60):
         sys.stdout.flush()
 
 
-def get_start_end_block_numbers(skale, node_ids, start_block=None, end_date=None):
-    if start_block is None:
-        start_block = datetime.utcfromtimestamp(get_start_block(node_ids[0]))
+def get_start_end_block_numbers(skale, node_ids, start_date=None, end_date=None):
+    if start_date is None:
+        start_block_number = datetime.utcfromtimestamp(get_start_block(node_ids[0]))
+    else:
+        start_block_number = find_block_for_tx_stamp(skale, start_date)
 
-    start_block_number = find_block_for_tx_stamp(skale, start_block)
-    cur_block_number = skale.web3.eth.blockNumber
-    last_block_number = find_block_for_tx_stamp(skale, end_date) if end_date is not None \
-        else cur_block_number
+    if start_date is None:
+        last_block_number = skale.web3.eth.blockNumber
+    else:
+        last_block_number = find_block_for_tx_stamp(skale, end_date)
 
     return start_block_number, last_block_number
 
@@ -63,14 +65,14 @@ def format_limit(limit):
         return int(limit)
 
 
-def get_metrics_from_events(node_ids, start_block=None, end_date=None,
+def get_metrics_from_events(node_ids, start_date=None, end_date=None,
                             limit=None, is_validator=False):
     skale = init_skale_from_config()
     metrics_rows = []
     total_bounty = 0
     limit = format_limit(limit)
     start_block_number, last_block_number = get_start_end_block_numbers(skale, node_ids,
-                                                                        start_block, end_date)
+                                                                        start_date, end_date)
     start_chunk_block_number = start_block_number
     blocks_total = last_block_number - start_block_number
     while len(metrics_rows) < limit:
@@ -111,14 +113,14 @@ def get_metrics_from_events(node_ids, start_block=None, end_date=None,
     return metrics_rows, total_bounty
 
 
-def get_bounty_from_events(node_ids, start_block=None, end_date=None, limit=None) -> tuple:
+def get_bounty_from_events(node_ids, start_date=None, end_date=None, limit=None) -> tuple:
     skale = init_skale_from_config()
     bounty_rows = []
     total_bounty = 0
     cur_month_record = {}
     limit = format_limit(limit)
     start_block_number, last_block_number = get_start_end_block_numbers(skale, node_ids,
-                                                                        start_block, end_date)
+                                                                        start_date, end_date)
     start_chunk_block_number = start_block_number
     blocks_total = last_block_number - start_block_number
 
