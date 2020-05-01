b5add5611eceb37ce4c5a43aef441ce391796798
Alex Sheverdin <alex@skalelabs.com>
2020-05-01 14:45:27 +0300

    SKALE-2491 Change indents in test output

--- tests/cli/bounty_validator_test.py
+++ tests/cli/bounty_validator_test.py
@@ -40,7 +40,7 @@ def test_bounty(skale, runner):
     output_list = result.output.splitlines()[-row_count:]
     assert '      Date          All nodes   Node ID = 0   Node ID = 1' == output_list[0]
     assert '---------------------------------------------------------' == output_list[1]
-    assert f'      {metrics[0][0]}     {metrics[0][1]:.3f}       {metrics[0][2]:.3f}         {metrics[0][3]:.3f}' == output_list[2]  # noqa
+    assert f'      {metrics[0][0]}     {metrics[0][1]:.3f}       {metrics[0][2]:.3f}       {metrics[0][3]:.3f}' == output_list[2]  # noqa
 
 
 def test_metrics_since_till_limited_not_empty(skale, runner):
@@ -58,7 +58,7 @@ def test_metrics_since_till_limited_not_empty(skale, runner):
     print(output_list[2])
     assert '      Date          All nodes   Node ID = 0   Node ID = 1' == output_list[0]
     assert '---------------------------------------------------------' == output_list[1]
-    assert f'      {metrics[0][0]}     {metrics[0][1]:.3f}       {metrics[0][2]:.3f}         {metrics[0][3]:.3f}' == output_list[2]  # noqa
+    assert f'      {metrics[0][0]}     {metrics[0][1]:.3f}       {metrics[0][2]:.3f}       {metrics[0][3]:.3f}' == output_list[2]  # noqa
 
 
 def test_metrics_since_till_limited_empty(runner):
