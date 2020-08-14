061ff900670cfae9622165a3bcf35a39060d4bfa
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-08-14 17:36:28 +0300

    SKALE-3060 Add _send_skl and _send_eth functionality

--- cli/wallet.py
+++ cli/wallet.py
@@ -1,9 +1,15 @@
+import logging
 import click
+from skale.utils.web3_utils import to_checksum_address
+
 from utils.texts import Texts
+from utils.web3_utils import init_skale_from_config
+from skale.utils.account_tools import send_ether, send_tokens
 
 G_TEXTS = Texts()
 TEXTS = G_TEXTS['wallet']
-MSGS = G_TEXTS['msg']
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -20,11 +26,29 @@ def wallet():
 @click.argument('receiver_address')
 @click.argument('amount')
 def _send_eth(receiver_address, amount):
-    pass
+    skale = init_skale_from_config()
+    try:
+        receiver_address = to_checksum_address(receiver_address)
+        send_ether(skale.web3, skale.wallet, receiver_address, amount)
+        msg = TEXTS['send_eth']['success']
+        logger.info(msg)
+        print(msg)
+    except Exception as err:
+        logger.error('Funds were not sent due to error', exc_info=err)
+        print(TEXTS['send_eth']['error'])
 
 
 @wallet.command('send-skl', help=TEXTS['send_skl']['help'])
 @click.argument('receiver_address')
 @click.argument('amount')
-def _send_eth(receiver_address, amount):
-    pass
+def _send_skl(receiver_address, amount):
+    skale = init_skale_from_config()
+    try:
+        receiver_address = to_checksum_address(receiver_address)
+        send_tokens(skale, skale.wallet, receiver_address, amount)
+        msg = TEXTS['send_skl']['success']
+        logger.info(msg)
+        print(msg)
+    except Exception as err:
+        logger.error('Funds were not sent due to error', exc_info=err)
+        print(TEXTS['send_skl']['error'])
