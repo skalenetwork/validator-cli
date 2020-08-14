a5e88cdd33178fa3e5487d1fe4df837f1a4b71ad
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-08-14 18:02:43 +0300

    SKALE-3060 Add tests for _send_skl & _send_eth

---
+++ tests/cli/wallet_test.py
@@ -0,0 +1,26 @@
+from cli.wallet import _send_eth, _send_skl
+
+
+def test_send_eth(runner, skale):
+    address = skale.wallet.address
+    balance_0 = skale.web3.eth.getBalance(address)
+    result = runner.invoke(
+        _send_eth,
+        ['0x01C19c5d3Ad1C3014145fC82263Fbae09e23924A', '0.01']
+    )
+    balance_1 = skale.web3.eth.getBalance(address)
+    assert balance_1 < balance_0
+
+    output_list = result.output.splitlines()
+    assert result.exit_code == 0
+    assert output_list == ['Funds were successfully transferred']
+
+
+def test_send_skl(runner, skale):
+    result = runner.invoke(
+        _send_skl,
+        ['0x01C19c5d3Ad1C3014145fC82263Fbae09e23924A', '0.01']
+    )
+    output_list = result.output.splitlines()
+    assert result.exit_code == 0
+    assert output_list == ['Funds were successfully transferred']
