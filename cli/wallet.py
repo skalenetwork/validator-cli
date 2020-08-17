dc5cb8cd24c063c7e96162f6d9c8e3c8d45ae661
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-08-17 12:36:57 +0300

    SKALE-3060 Retrieve wallet logic into core/wallet.py

--- cli/wallet.py
+++ cli/wallet.py
@@ -1,11 +1,9 @@
 import logging
 import click
-from skale.utils.web3_utils import to_checksum_address
 
+from core.wallet import send_eth, send_skl
 from utils.helper import abort_if_false
 from utils.texts import Texts
-from utils.web3_utils import init_skale_w_wallet_from_config
-from skale.utils.account_tools import send_ether, send_tokens
 
 G_TEXTS = Texts()
 TEXTS = G_TEXTS['wallet']
@@ -33,16 +31,7 @@ def wallet():
 @click.option('--yes', is_flag=True, callback=abort_if_false,
               expose_value=False, prompt=TEXTS['send_eth']['confirm'])
 def _send_eth(receiver_address, amount, pk_file):
-    skale = init_skale_w_wallet_from_config(pk_file)
-    try:
-        receiver_address = to_checksum_address(receiver_address)
-        send_ether(skale.web3, skale.wallet, receiver_address, amount)
-        msg = TEXTS['send_eth']['success']
-        logger.info(msg)
-        print(msg)
-    except Exception as err:
-        logger.error('Funds were not sent due to error', exc_info=err)
-        print(TEXTS['send_eth']['error'])
+    send_eth(receiver_address, amount, pk_file)
 
 
 @wallet.command('send-skl', help=TEXTS['send_skl']['help'])
@@ -55,13 +44,4 @@ def _send_eth(receiver_address, amount, pk_file):
 @click.option('--yes', is_flag=True, callback=abort_if_false,
               expose_value=False, prompt=TEXTS['send_skl']['confirm'])
 def _send_skl(receiver_address, amount, pk_file):
-    skale = init_skale_w_wallet_from_config(pk_file)
-    try:
-        receiver_address = to_checksum_address(receiver_address)
-        send_tokens(skale, skale.wallet, receiver_address, amount)
-        msg = TEXTS['send_skl']['success']
-        logger.info(msg)
-        print(msg)
-    except Exception as err:
-        logger.error('Funds were not sent due to error', exc_info=err)
-        print(TEXTS['send_skl']['error'])
+    send_skl(receiver_address, amount, pk_file)
