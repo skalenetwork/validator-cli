21f58ea22d8ecd12f3e978c1653aa4bcaa0b5a65
badrogger <ivan@skalelabs.com>
2022-10-17 09:55:13 +0000

    SKALE-4904 Fix reverted exit code test

--- tests/exit_codes_test.py
+++ tests/exit_codes_test.py
@@ -3,6 +3,7 @@ import subprocess
 from subprocess import PIPE
 
 from tests.constants import TEST_PK_FILE
+from skale.utils.account_tools import generate_account
 
 
 def test_no_such_command_exit_code(executable):
@@ -11,10 +12,13 @@ def test_no_such_command_exit_code(executable):
     assert result.returncode == 2
 
 
-def test_reverted_exit_code(executable):
+def test_reverted_exit_code(skale, executable):
+    # generate new address
+    addr = generate_account(skale.web3)['address']
+    # try to unlink not linked address
     cmd = [
-        executable, 'validator', 'register', '-n', 'test', '-d', 'test',
-        '--commission-rate', '0.1', '--min-delegation', '1000', '--pk-file', TEST_PK_FILE, '--yes'
+        executable, 'validator', 'unlink-address', addr,
+        '--pk-file', TEST_PK_FILE, '--yes'
     ]
     result = subprocess.run(cmd, shell=False, stdout=PIPE, stderr=PIPE, env={**os.environ})
     print(result.stdout, result.returncode)
